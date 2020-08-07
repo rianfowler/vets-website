f0046b33782a328f892cba6c4ac9e90e9298f9de
mr0sari0 <mrosario@governmentcio.com>
2020-08-07 15:10:52 -0400

    remove obj default

--- src/platform/forms/components/GetPensionOrBurialFormHelp.jsx
+++ src/platform/forms/components/GetPensionOrBurialFormHelp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CallVBACenter from '../../static-data/CallVBACenter';
 import { APP_TYPE_DEFAULT } from 'platform/forms-system/src/js/constants';
 
-function GetFormHelp(formConfig = {}) {
+function GetFormHelp(formConfig) {
   const appType = formConfig?.customText?.appType ?? APP_TYPE_DEFAULT;
   return (
     <div>
