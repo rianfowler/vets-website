4ee96ed85d135a5907ef396b4507063554c15df0
mr0sari0 <mrosario@governmentcio.com>
2020-08-07 15:12:10 -0400

    remove obj default

--- src/platform/forms/save-in-progress/DowntimeMessage.jsx
+++ src/platform/forms/save-in-progress/DowntimeMessage.jsx
@@ -6,7 +6,7 @@ import { APP_TYPE_DEFAULT } from '../../forms-system/src/js/constants';
 export default function DowntimeMessage({
   downtime,
   isAfterSteps,
-  formConfig = {},
+  formConfig,
 }) {
   const endTime = downtime.endTime;
   const appType = formConfig?.customText?.appType ?? APP_TYPE_DEFAULT;
